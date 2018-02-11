5fb38892dc3cc74692b169beb3e658491815e97c
Michał <michalmichalik25@gmail.com>
2018-02-11 12:56:37 +0100

    Changed exception message

--- src/Slownie.php
+++ src/Slownie.php
@@ -5,7 +5,7 @@ class Slownie
     public function liczba($number)
     {
         if (is_numeric($number) === false) {
-            throw new Exception("Podany argument nie jest liczbą");
+            throw new Exception("Passed parameter isn't numeric");
         }
 
         if ($number == 0) {
