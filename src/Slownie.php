9670a9ef2bab58cd712479e9f79a688ee71fe18b
Michał <michalmichalik25@gmail.com>
2018-02-19 09:48:11 +0100

    Move some method 'integerToText' and 'fractionToText' variables to class scope as private fields

--- src/Slownie.php
+++ src/Slownie.php
@@ -2,6 +2,103 @@
 
 class Slownie
 {
+    private $singles   = ['', ' jeden', ' dwa', ' trzy', ' cztery', ' pięć', ' sześć', ' siedem', ' osiem', ' dziewięć'];
+    private $teens     = ['', ' jedenaście', ' dwanaście', ' trzynaście', ' czternaście', ' piętnaście', ' szesnaście', ' siedemnaście', ' osiemnaście', ' dziewietnaście'];
+    private $tens      = ['', ' dziesieć', ' dwadzieścia', ' trzydzieści', ' czterdzieści', ' pięćdziesiąt', ' sześćdziesiąt', ' siedemdziesiąt', ' osiemdziesiąt', ' dziewięćdziesiąt'];
+    private $hundreds  = ['', ' sto', ' dwieście', ' trzysta', ' czterysta', ' pięćset', ' sześćset', ' siedemset', ' osiemset', ' dziewięćset'];
+    private $groups    = [
+        [''             ,''             ,''              ],
+        [' tysiąc'      ,' tysiące'     ,' tysięcy'      ],
+        [' milion'      ,' miliony'     ,' milionów'     ],
+        [' miliard'     ,' miliardy'    ,' miliardów'    ],
+        [' bilion'      ,' biliony'     ,' bilionów'     ],
+        [' biliard'     ,' biliardy'    ,' biliardów'    ],
+        [' trylion'     ,' tryliony'    ,' trylionów'    ],
+        [' tryliard'    ,' tryliardy'   ,' tryliardów'   ],
+        [' kwadrylion'  ,' kwadryliony' ,' kwadrylionów' ],
+        [' kwadryliard' ,' kwadryliardy',' kwadryliardów'],
+        [' kwintylion'  ,' kwintyliony' ,' kwintylionów' ],
+        [' kwintyliard' ,' kwintyliardy',' kwintyliardów'],
+        [' sekstylion'  ,' sekstyliony' ,' sektylionów'  ],
+        [' sekstyliard' ,' sektyliardy' ,' sekstyliardów'],
+        [' septylion'   ,' septyliony'  ,' septylionów'  ],
+        [' septyliard'  ,' septyliardy' ,' septyliardów' ],
+        [' oktylion'    ,' oktyliony'   ,' oktylionów'   ],
+        [' oktyliard'   ,' oktyliardy'  ,' oktyliardów'  ],
+        [' nonylion'    ,' nonyliony'   ,' nonylionów'   ],
+        [' nonyliard'   ,' nonyliardy'  ,' nonyliardów'  ],
+        [' decylion'    ,' decyliony'   ,' decylionów'   ],
+        [' decyliard'   ,' decyliardy'  ,' decyliardów'  ]
+    ];
+    private $groupsFraction = [
+        [''                         ,''                         ,''                           ],
+        [' dziesiąta'               ,' dziesiąte'               ,' dziesiątych'               ],
+        [' setna'                   ,' setne'                   ,' setnych'                   ],
+        [' tysięczna'               ,' tysięczne'               ,' tysięcznych'               ],
+        [' dziesięciotysięczna'     ,' dziesięciotysięczne'     ,' dziesięciotysięcznych'     ],
+        [' stutysięczna'            ,' stutysięczne'            ,' stutysięcznych'            ],
+        [' milionowa'               ,' milionowe'               ,' milionowych'               ],
+        [' dziesięciomilionowa'     ,' dziesięciomilionowe'     ,' dziesięciomilionowych'     ],
+        [' stumilionowa'            ,' stumilionowe'            ,' stumilionowych'            ],
+        [' miliardowa'              ,' miliardowe'              ,' miliardowych'              ],
+        [' dziesięciomiliardowa'    ,' dziesięciomiliardowe'    ,' dzięsieciomiliardowych'    ],
+        [' stumiliardowa'           ,' stumiliardowe'           ,' stumiliardowych'           ],
+        [' bilionowa'               ,' bilionowe'               ,' bilionowych'               ],
+        [' dzięsięciobilionowa'     ,' dzięsięciobilionowe'     ,' dzięsięciobilionowych'     ],
+        [' stubilionowa'            ,' stubilionowe'            ,' stubilionowych'            ],
+        [' biliardowa'              ,' biliardowe'              ,' biliardowych'              ],
+        [' dzięsięciobiliardowa'    ,' dzięsięciobiliardowe'    ,' dzięsięciobiliardowych'    ],
+        [' stubiliardowa'           ,' stubiliardowe'           ,' stubiliardowych'           ],
+        [' trylionowa'              ,' trylionowe'              ,' trylionowych'              ],
+        [' dziesięciotrylionowa'    ,' dziesięciotrylionowe'    ,' dziesięciotrylionowych'    ],
+        [' stutrylionowa'           ,' stutrylionowe'           ,' stutrylionowych'           ],
+        [' tryliardowa'             ,' tryliardowe'             ,' tryliardowych'             ],
+        [' dzięsięciotryliardowa'   ,' dzięsięciotryliardowe'   ,' dzięsięciotryliardowych'   ],
+        [' stutryliardowa'          ,' stutryliardowe'          ,' stutryliardowych'          ],
+        [' kwadrylionowa'           ,' kwadrylionowe'           ,' kwadrylionowych'           ],
+        [' dzięsięciokwadrylionowa' ,' dzięsięciokwadrylionowe' ,' dzięsięciokwadrylionowych' ],
+        [' stukwadrylionowa'        ,' stukwadrylionowe'        ,' stukwadrylionowych'        ],
+        [' kwadryliardowa'          ,' kwadryliardowe'          ,' kwadryliardowych'          ],
+        [' dzięsięciokwadryliardowa',' dzięsięciokwadryliardowe',' dzięsięciokwadryliardowych'],
+        [' stukwadryliardowa'       ,' stukwadryliardowe'       ,' stukwadryliardowych'       ],
+        [' kwintylionowa'           ,' kwintylionowe'           ,' kwintylionowych'           ],
+        [' dzięsięciokwintylionowa' ,' dzięsięciokwintylionowe' ,' dzięsięciokwintylionowych' ],
+        [' stukwintylionowa'        ,' stukwintylionowe'        ,' stukwintylionowych'        ],
+        [' kwintyliardowa'          ,' kwintyliardowe'          ,' kwintyliardowych'          ],
+        [' dzięsięciokwintyliardowa',' dzięsięciokwintyliardowe',' dzięsięciokwintyliardowych'],
+        [' stukwintyliardowa'       ,' stukwintyliardowe'       ,' stukwintyliardowych'       ],
+        [' sekstylionowa'           ,' sekstylionowe'           ,' sekstylionowych'           ],
+        [' dziesięciosekstylionowa' ,' dziesięciosekstylionowe' ,' dziesięciosekstylionowych' ],
+        [' stusekstylionowa'        ,' stusekstylionowe'        ,' stusekstylionowych'        ],
+        [' sekstyliardowa'          ,' sekstyliardowe'          ,' sekstyliardowych'          ],
+        [' dziesięciosekstyliardowa',' dziesięciosekstyliardowe',' dziesięciosekstyliardowych'],
+        [' stusekstyliardowa'       ,' stusekstyliardowe'       ,' stusekstyliardowych'       ],
+        [' septylionowa'            ,' septylionowe'            ,' septylionowych'            ],
+        [' dziesięcioseptylionowa'  ,' dziesięcioseptylionowe'  ,' dziesięcioseptylionowych'  ],
+        [' stuseptylionowa'         ,' stuseptylionowe'         ,' stuseptylionowych'         ],
+        [' septyliardowa'           ,' septyliardowe'           ,' septyliardowych'           ],
+        [' dziesięcioseptyliardowa' ,' dziesięcioseptyliardowe' ,' dziesięcioseptyliardowych' ],
+        [' stuseptyliardowa'        ,' stuseptyliardowe'        ,' stuseptyliardowych'        ],
+        [' oktylionowa'             ,' oktylionowe'             ,' oktylionowych'             ],
+        [' dziesięciooktylionowa'   ,' dziesięciooktylionowe'   ,' dziesięciooktylionowych'   ],
+        [' stuoktylionowa'          ,' stuoktylionowe'          ,' stuoktylionowych'          ],
+        [' oktyliardowa'            ,' oktyliardowe'            ,' oktyliardowych'            ],
+        [' dziesięciooktyliardowa'  ,' dziesięciooktyliardowe'  ,' dziesięciooktyliardowych'  ],
+        [' stuoktyliardowa'         ,' stuoktyliardowe'         ,' stuoktyliardowych'         ],
+        [' nonylionowa'             ,' nonylionowe'             ,' nonylionowych'             ],
+        [' dziesięciononylionowa'   ,' dziesięciononylionowe'   ,' dziesięciononylionowych'   ],
+        [' stunonylionowa'          ,' stunonylionowe'          ,' stunonylionowych'          ],
+        [' nonyliardowa'            ,' nonyliardowe'            ,' nonyliardowych'            ],
+        [' dziesięciononyliardowa'  ,' dziesięciononyliardowe'  ,' dziesięciononyliardowych'  ],
+        [' stunonyliardowa'         ,' stunonyliardowe'         ,' stunonyliardowych'         ],
+        [' decylionowa'             ,' decylionowe'             ,' decylionowych'             ],
+        [' dziesięciodecylionowa'   ,' dziesięciodecylionowe'   ,' dziesięciodecylionowych'   ],
+        [' studecylionowa'          ,' studecylionowe'          ,' studecylionowych'          ],
+        [' decyliardowa'            ,' decyliardowe'            ,' decyliardowych'            ],
+        [' dziesięciodecyliardowa'  ,' dziesięciodecyliardowe'  ,' dziesięciodecyliardowych'  ],
+        [' studecyliardowa'         ,' studecyliardowe'         ,' studecyliardowych'         ]
+    ];
+
     public function liczba($number)
     {
         if (is_numeric($number) === false) {
@@ -53,35 +150,6 @@ class Slownie
             }
         }
 
-        $singles   = ['', ' jeden', ' dwa', ' trzy', ' cztery', ' pięć', ' sześć', ' siedem', ' osiem', ' dziewięć'];
-        $teens     = ['', ' jedenaście', ' dwanaście', ' trzynaście', ' czternaście', ' piętnaście', ' szesnaście', ' siedemnaście', ' osiemnaście', ' dziewietnaście'];
-        $tens      = ['', ' dziesieć', ' dwadzieścia', ' trzydzieści', ' czterdzieści', ' pięćdziesiąt', ' sześćdziesiąt', ' siedemdziesiąt', ' osiemdziesiąt', ' dziewięćdziesiąt'];
-        $hundreds  = ['', ' sto', ' dwieście', ' trzysta', ' czterysta', ' pięćset', ' sześćset', ' siedemset', ' osiemset', ' dziewięćset'];
-        $groups    = [
-            [''             ,''             ,''              ],
-            [' tysiąc'      ,' tysiące'     ,' tysięcy'      ],
-            [' milion'      ,' miliony'     ,' milionów'     ],
-            [' miliard'     ,' miliardy'    ,' miliardów'    ],
-            [' bilion'      ,' biliony'     ,' bilionów'     ],
-            [' biliard'     ,' biliardy'    ,' biliardów'    ],
-            [' trylion'     ,' tryliony'    ,' trylionów'    ],
-            [' tryliard'    ,' tryliardy'   ,' tryliardów'   ],
-            [' kwadrylion'  ,' kwadryliony' ,' kwadrylionów' ],
-            [' kwadryliard' ,' kwadryliardy',' kwadryliardów'],
-            [' kwintylion'  ,' kwintyliony' ,' kwintylionów' ],
-            [' kwintyliard' ,' kwintyliardy',' kwintyliardów'],
-            [' sekstylion'  ,' sekstyliony' ,' sektylionów'  ],
-            [' sekstyliard' ,' sektyliardy' ,' sekstyliardów'],
-            [' septylion'   ,' septyliony'  ,' septylionów'  ],
-            [' septyliard'  ,' septyliardy' ,' septyliardów' ],
-            [' oktylion'    ,' oktyliony'   ,' oktylionów'   ],
-            [' oktyliard'   ,' oktyliardy'  ,' oktyliardów'  ],
-            [' nonylion'    ,' nonyliony'   ,' nonylionów'   ],
-            [' nonyliard'   ,' nonyliardy'  ,' nonyliardów'  ],
-            [' decylion'    ,' decyliony'   ,' decylionów'   ],
-            [' decyliard'   ,' decyliardy'  ,' decyliardów'  ]
-        ];
-
         $array = $this->numberToArray($number);
 
         $arrayLength = count($array);
@@ -106,9 +174,9 @@ class Slownie
             }
 
             if ($isTeens) {
-                $result .= $hundreds[$h] . $teens[$s] . $groups[$g][$grammarForm];
+                $result .= $this->hundreds[$h] . $this->teens[$s] . $this->groups[$g][$grammarForm];
             } else {
-                $result .= $hundreds[$h] . $tens[$t] . $singles[$s] . $groups[$g][$grammarForm];
+                $result .= $this->hundreds[$h] . $this->tens[$t] . $this->singles[$s] . $this->groups[$g][$grammarForm];
             }
         }
 
@@ -124,75 +192,6 @@ class Slownie
         if ($number == '') {
             return $number;
         }
-
-        $groups = [
-            [''                         ,''                         ,''                           ],
-            [' dziesiąta'               ,' dziesiąte'               ,' dziesiątych'               ],
-            [' setna'                   ,' setne'                   ,' setnych'                   ],
-            [' tysięczna'               ,' tysięczne'               ,' tysięcznych'               ],
-            [' dziesięciotysięczna'     ,' dziesięciotysięczne'     ,' dziesięciotysięcznych'     ],
-            [' stutysięczna'            ,' stutysięczne'            ,' stutysięcznych'            ],
-            [' milionowa'               ,' milionowe'               ,' milionowych'               ],
-            [' dziesięciomilionowa'     ,' dziesięciomilionowe'     ,' dziesięciomilionowych'     ],
-            [' stumilionowa'            ,' stumilionowe'            ,' stumilionowych'            ],
-            [' miliardowa'              ,' miliardowe'              ,' miliardowych'              ],
-            [' dziesięciomiliardowa'    ,' dziesięciomiliardowe'    ,' dzięsieciomiliardowych'    ],
-            [' stumiliardowa'           ,' stumiliardowe'           ,' stumiliardowych'           ],
-            [' bilionowa'               ,' bilionowe'               ,' bilionowych'               ],
-            [' dzięsięciobilionowa'     ,' dzięsięciobilionowe'     ,' dzięsięciobilionowych'     ],
-            [' stubilionowa'            ,' stubilionowe'            ,' stubilionowych'            ],
-            [' biliardowa'              ,' biliardowe'              ,' biliardowych'              ],
-            [' dzięsięciobiliardowa'    ,' dzięsięciobiliardowe'    ,' dzięsięciobiliardowych'    ],
-            [' stubiliardowa'           ,' stubiliardowe'           ,' stubiliardowych'           ],
-            [' trylionowa'              ,' trylionowe'              ,' trylionowych'              ],
-            [' dziesięciotrylionowa'    ,' dziesięciotrylionowe'    ,' dziesięciotrylionowych'    ],
-            [' stutrylionowa'           ,' stutrylionowe'           ,' stutrylionowych'           ],
-            [' tryliardowa'             ,' tryliardowe'             ,' tryliardowych'             ],
-            [' dzięsięciotryliardowa'   ,' dzięsięciotryliardowe'   ,' dzięsięciotryliardowych'   ],
-            [' stutryliardowa'          ,' stutryliardowe'          ,' stutryliardowych'          ],
-            [' kwadrylionowa'           ,' kwadrylionowe'           ,' kwadrylionowych'           ],
-            [' dzięsięciokwadrylionowa' ,' dzięsięciokwadrylionowe' ,' dzięsięciokwadrylionowych' ],
-            [' stukwadrylionowa'        ,' stukwadrylionowe'        ,' stukwadrylionowych'        ],
-            [' kwadryliardowa'          ,' kwadryliardowe'          ,' kwadryliardowych'          ],
-            [' dzięsięciokwadryliardowa',' dzięsięciokwadryliardowe',' dzięsięciokwadryliardowych'],
-            [' stukwadryliardowa'       ,' stukwadryliardowe'       ,' stukwadryliardowych'       ],
-            [' kwintylionowa'           ,' kwintylionowe'           ,' kwintylionowych'           ],
-            [' dzięsięciokwintylionowa' ,' dzięsięciokwintylionowe' ,' dzięsięciokwintylionowych' ],
-            [' stukwintylionowa'        ,' stukwintylionowe'        ,' stukwintylionowych'        ],
-            [' kwintyliardowa'          ,' kwintyliardowe'          ,' kwintyliardowych'          ],
-            [' dzięsięciokwintyliardowa',' dzięsięciokwintyliardowe',' dzięsięciokwintyliardowych'],
-            [' stukwintyliardowa'       ,' stukwintyliardowe'       ,' stukwintyliardowych'       ],
-            [' sekstylionowa'           ,' sekstylionowe'           ,' sekstylionowych'           ],
-            [' dziesięciosekstylionowa' ,' dziesięciosekstylionowe' ,' dziesięciosekstylionowych' ],
-            [' stusekstylionowa'        ,' stusekstylionowe'        ,' stusekstylionowych'        ],
-            [' sekstyliardowa'          ,' sekstyliardowe'          ,' sekstyliardowych'          ],
-            [' dziesięciosekstyliardowa',' dziesięciosekstyliardowe',' dziesięciosekstyliardowych'],
-            [' stusekstyliardowa'       ,' stusekstyliardowe'       ,' stusekstyliardowych'       ],
-            [' septylionowa'            ,' septylionowe'            ,' septylionowych'            ],
-            [' dziesięcioseptylionowa'  ,' dziesięcioseptylionowe'  ,' dziesięcioseptylionowych'  ],
-            [' stuseptylionowa'         ,' stuseptylionowe'         ,' stuseptylionowych'         ],
-            [' septyliardowa'           ,' septyliardowe'           ,' septyliardowych'           ],
-            [' dziesięcioseptyliardowa' ,' dziesięcioseptyliardowe' ,' dziesięcioseptyliardowych' ],
-            [' stuseptyliardowa'        ,' stuseptyliardowe'        ,' stuseptyliardowych'        ],
-            [' oktylionowa'             ,' oktylionowe'             ,' oktylionowych'             ],
-            [' dziesięciooktylionowa'   ,' dziesięciooktylionowe'   ,' dziesięciooktylionowych'   ],
-            [' stuoktylionowa'          ,' stuoktylionowe'          ,' stuoktylionowych'          ],
-            [' oktyliardowa'            ,' oktyliardowe'            ,' oktyliardowych'            ],
-            [' dziesięciooktyliardowa'  ,' dziesięciooktyliardowe'  ,' dziesięciooktyliardowych'  ],
-            [' stuoktyliardowa'         ,' stuoktyliardowe'         ,' stuoktyliardowych'         ],
-            [' nonylionowa'             ,' nonylionowe'             ,' nonylionowych'             ],
-            [' dziesięciononylionowa'   ,' dziesięciononylionowe'   ,' dziesięciononylionowych'   ],
-            [' stunonylionowa'          ,' stunonylionowe'          ,' stunonylionowych'          ],
-            [' nonyliardowa'            ,' nonyliardowe'            ,' nonyliardowych'            ],
-            [' dziesięciononyliardowa'  ,' dziesięciononyliardowe'  ,' dziesięciononyliardowych'  ],
-            [' stunonyliardowa'         ,' stunonyliardowe'         ,' stunonyliardowych'         ],
-            [' decylionowa'             ,' decylionowe'             ,' decylionowych'             ],
-            [' dziesięciodecylionowa'   ,' dziesięciodecylionowe'   ,' dziesięciodecylionowych'   ],
-            [' studecylionowa'          ,' studecylionowe'          ,' studecylionowych'          ],
-            [' decyliardowa'            ,' decyliardowe'            ,' decyliardowych'            ],
-            [' dziesięciodecyliardowa'  ,' dziesięciodecyliardowe'  ,' dziesięciodecyliardowych'  ],
-            [' studecyliardowa'         ,' studecyliardowe'         ,' studecyliardowych'         ]
-        ];
 
         $array = $this->numberToArray($number);
         $arrayLength = count($array);
@@ -228,7 +227,7 @@ class Slownie
             $result = $this->integerToText($number);
         }
 
-        $result .= $groups[$numberLength][$grammarForm];
+        $result .= $this->groupsFraction[$numberLength][$grammarForm];
 
         return $result;
     }
